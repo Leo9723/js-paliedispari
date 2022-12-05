2c17f7c22703ea45f17d45d37fd4f551423e6e58
Leo9723 <pala9761@gmail.com>
2022-12-05 17:46:31 +0100

    aggiunta bonus esercizio pari e dispari

--- dispari/js/script.js
+++ dispari/js/script.js
@@ -1,51 +1,56 @@
+let button = document.getElementById('button')
 
-
-function randomNumberEsit (number) {
-    let pc_number = parseInt(Math.floor(Math.random() * 5 + 1))
-    final_number = number + pc_number
-    console.log(pc_number)
-    console.log(final_number)
-    return final_number
-}
-
-
-let user_choice = prompt('scegli tra pari e dispari')
-
-if (user_choice == 'pari') {
-    pari = true
-}
-else {
-    pari = false
-}
-
-let user_number = parseInt(prompt('inserisci un numero da 1 a 5'))
-
-let final_esit = randomNumberEsit (user_number)
-console.log(pari)
-
-let esit = true;
-
-
-if (final_esit % 2 != 0) {
-    esit = false
-}
-console.log(esit)
-
-if (esit == true) {
-    if (pari === true) {
-        alert('hai vinto')
+button.addEventListener('click', function() {
+    
+    function randomNumberEsit (number) {
+        let pc_number = parseInt(Math.floor(Math.random() * 5 + 1))
+        final_number = number + pc_number
+        console.log(pc_number)
+        console.log(final_number)
+        return final_number
+    }
+    
+    
+    
+    let user_choice = document.getElementById('pari-dispari').value
+    
+    if (user_choice == 'pari') {
+        pari = true
     }
     else {
-        alert('hai perso')
+        pari = false
     }
-}
+    
+    let user_number = parseInt(document.getElementById('user-number').value)
+    
+    let final_esit = randomNumberEsit (user_number)
+    console.log(pari)
+    
+    let esit = true;
+    
+    
+    if (final_esit % 2 != 0) {
+        esit = false
+    }
+    console.log(esit)
+    
+    if (esit == true) {
+        if (pari === true) {
+            document.getElementById("final-score").innerHTML = 'hai vinto'
+        }
+        else {
+            document.getElementById("final-score").innerHTML = 'hai perso'
+        }
+    }
+    
+    else if (esit == false) {
+        if (pari === false) {
+            document.getElementById("final-score").innerHTML = 'hai vinto'
+        }
+        else {
+            document.getElementById("final-score").innerHTML = 'hai perso'
+        }
+    }
 
-else if (esit == false) {
-    if (pari === false) {
-        alert('hai vinto')
-    }
-    else {
-        alert('hai perso')
-    }
-}
+})
 
--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,26 +1,34 @@
-let esit = false
-
 function paliword(word) {
     let new_word = word.split('')
         new_word = new_word.reverse('')
         new_word = new_word.join('')
-    console.log(new_word)
-    
-    if (word == new_word) {
-        esit = true
-    }
-    console.log(esit)
+        console.log(new_word)
+        return new_word
+}
+
+let user_word = prompt('inserisci una parola')
+
+let final_esit = paliword(user_word)
+
+let esit = true
+
+if (user_word == final_esit) {
+    esit == true
+}
+
+else {
+    esit == false
 }
 
 console.log(esit)
 
-let user_word = prompt('inserisci una parola')
-
-paliword(user_word)
-
-if (esit = true) {
+if (esit == true) {
     alert('la parola inserita è palindroma')
 }
 else {
     alert('la parola inserita NON è palindroma')
 }
+
+console.log(final_esit)
+
+console.log(user_word)
