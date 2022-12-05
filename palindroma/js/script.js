e07ca8b7722003d85546ec9f2760fc639a60f445
Leo9723 <pala9761@gmail.com>
2022-12-05 15:51:53 +0100

    push parziale del primo esercizio

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,3 +1,26 @@
+let esit = false
+
 function paliword(word) {
+    let new_word = word.split('')
+        new_word = new_word.reverse('')
+        new_word = new_word.join('')
+    console.log(new_word)
     
+    if (word == new_word) {
+        esit = true
+    }
+    console.log(esit)
+}
+
+console.log(esit)
+
+let user_word = prompt('inserisci una parola')
+
+paliword(user_word)
+
+if (esit = true) {
+    alert('la parola inserita è palindroma')
+}
+else {
+    alert('la parola inserita NON è palindroma')
 }
